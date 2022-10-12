b9f3113ae379de6a84c09c29d77e03a072737cf5
ahmedreghini <59826888+ahmedreghini@users.noreply.github.com>
2022-10-12 14:35:42 +0100

    Update getStableValue.h

--- getStableValue.h
+++ getStableValue.h
@@ -1,6 +1,17 @@
+/*
+  getStableValue.h - Library for get stable value from esp32 or arduino ADC input.
+  Created by Ahmed REGHINI, 2022.
+  Released into the public domain.
+*/
+
+#ifndef Morse_h
+#define Morse_h
+
+#include "Arduino.h"
+
 class getStableValue
 {
 Public:
-getStableValue(int )
-
-}
+getStableValue(uint8_t pin, int nvs = 100, int x = 4, int dep = 2);
+};
+#endif
